afe31ce537d126e6eb0a760db657b3116706e520
LucasMoce <lmocellingoulart@gmail.com>
2023-02-24 15:46:19 -0300

    Nova versao do Sidescan

---
+++ README.md
@@ -0,0 +1,20 @@
+# Sidescan
+
+## Installing
+
+```
+$ git clone git@github.com:omixlab/sidescan.git
+$ cd sidescan
+$ conda env create
+$ conda activate sidescan
+```
+
+## Download datasets
+
+Downloads data from the SIDERS (side effects database) to the `data/` directory.
+
+```
+$ sidescan-download data/
+```
+
+## Preprocess datasets
--- sidescan/server.py
+++ sidescan/server.py
@@ -1,4 +1,33 @@
+from flask import Flask, render_template, session, redirect, url_for, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'chaveboa'
+
+class NameForm(FlaskForm):
+    molecule = StringField( validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        session['name'] = form.molecule.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
+
+@app.route('/upload', methods=['post'])
+def upload():
+    form = NameForm()
+    if form.validate_on_submit():
+        print(form.molecule)
+    return render_template('job.html', molecule=form.molecule.data)
+
+
 def main():
+    app.run()
     return
 
 if __name__ == '__main__':
